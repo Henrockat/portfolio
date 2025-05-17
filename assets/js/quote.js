1a1351b76120c7298da60f0e135b040a75e99eee
Henrockat <henawfunnz@hotmail.com>
2025-05-17 20:56:17 +0200

    Delete assets/js directory

--- assets/js/quote.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random'))
-  .then(res => res.json())
-  .then(data => {
-    const parsed = JSON.parse(data.contents);
-    document.getElementById('quote').innerText = `"${parsed[0].q}" — ${parsed[0].a}`;
-  })
-  .catch(() => {
-    document.getElementById('quote').innerText = 'Kon geen quote laden.';
-  });
